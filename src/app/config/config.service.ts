11c053e33d8b0ca32b561b280631d0ee42f57997
Andrea Kittner <kittner13@googlemail.com>
2018-04-29 08:36:14 +0200

    fixed services

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -2,8 +2,6 @@ import {Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Pflegekunde} from '../obj/pflegekunde';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ConfigService {
