c04daeacdfe229f075a03a8bc5001376f8df4019
Andrea Kittner <kittner13@googlemail.com>
2018-04-29 11:43:41 +0200

    Object files

---
+++ src/app/obj/Answer.ts
@@ -0,0 +1,4 @@
+import {Item} from './protocoll'
+export class Answer{
+ Item:Item;
+}
---
+++ src/app/obj/protocoll.ts
@@ -0,0 +1,8 @@
+export class Item{
+        Protocol: string;
+       PatientName: string;
+       BowelMovment: boolean;
+        LiquidIntake: number;
+        MedicalIntake: boolean;
+       PatientId: number;
+        }
